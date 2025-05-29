77bee8a4373873b70317e95e07176e4a6a066611
Nolita <159141178+NoeStorg4to@users.noreply.github.com>
2025-05-29 09:56:07 -0300

    Add files via upload

---
+++ librosEspeciales.js
@@ -0,0 +1,172 @@
+db.librosEspeciales.insertMany ([
+    {
+        "titulo": "El viajero",
+        "genero": "aventura",
+        "autores": ["Juan Pérez", "Lalo Landa"],
+        "numeroPaginas": 320,
+        "nivelLectura": "juvenil",
+        "precios": [
+            {"tipo": "fisico", "precio": 3500},
+            {"tipo": "digital", "precio": 2500}   
+        ],
+        "disponibleStock": true,
+        "editorial": {
+            "nombre": "Aventuras Editoriales",
+            "pais": "España",
+            "anioFundacion": 1995
+        }
+    },
+    {
+        "titulo": "Estrellas perdidas",
+        "genero": "ciencia ficcion",
+        "autores": ["Carlos Gómez"],
+        "numeroPaginas": 480,
+        "nivelLectura": "adulto",
+        "precios": [
+            {"tipo": "fisico", "precio": 4200},
+            {"tipo": "digital", "precio": 3000}   
+        ],
+        "disponibleStock": true,
+        "editorial": {
+            "nombre": "Galaxia Libros",
+            "pais": "Mexico",
+            "anioFundacion": 2005
+        }
+    },
+    {
+        "titulo": "Amor en verano",
+        "genero": "romance",
+        "autores": ["Lucía Herrera"],
+        "numeroPaginas": 280,
+        "nivelLectura": "juvenil",
+        "precios": [
+            {"tipo": "fisico", "precio": 3800},
+            {"tipo": "digital", "precio": 2800}   
+        ],
+        "disponibleStock": false,
+        "editorial": {
+            "nombre": "Romances Editoriales",
+            "pais": "Atgentina",
+            "anioFundacion": 2010
+        }
+    },
+    {
+        "titulo": "La casa del miedo",
+        "genero": "terror",
+        "autores": ["Lucía Herrera"],
+        "numeroPaginas": 310,
+        "nivelLectura": "adulto",
+        "precios": [
+            {"tipo": "fisico", "precio": 4000},
+            {"tipo": "digital", "precio": 2900}   
+        ],
+        "disponibleStock": true,
+        "editorial": {
+            "nombre": "Tinieblas",
+            "pais": "Chile",
+            "anioFundacion": 1985
+        }
+    },
+    {
+        "titulo": "El jardín secreto",
+        "genero": "aventura",
+        "autores": ["Francisco Hernandez"],
+        "numeroPaginas": 220,
+        "nivelLectura": "infantil",
+        "precios": [
+            {"tipo": "fisico", "precio": 3000},
+            {"tipo": "digital", "precio": 2000}   
+        ],
+        "disponibleStock": false,
+        "editorial": {
+            "nombre": "Aventuras Editoriales",
+            "pais": "Argentina",
+            "anioFundacion": 2010
+        }
+    },
+    {
+        "titulo": "Marte 2050",
+        "genero": "ciencia ficcion",
+        "autores": ["Carlos Gómez"],
+        "numeroPaginas": 520,
+        "nivelLectura": "adulto",
+        "precios": [
+            {"tipo": "fisico", "precio": 4500},
+            {"tipo": "digital", "precio": 3200}   
+        ],
+        "disponibleStock": true,
+        "editorial": {
+            "nombre": "Galaxia Libros",
+            "pais": "España",
+            "anioFundacion": 2015
+        }
+    },
+    {
+        "titulo": "Bajo la misma estrella",
+        "genero": "romance",
+        "autores": ["Sofia Castro"],
+        "numeroPaginas": 350,
+        "nivelLectura": "juvenil",
+        "precios": [
+            {"tipo": "fisico", "precio": 3700},
+            {"tipo": "digital", "precio": 2700}   
+        ],
+        "disponibleStock": false,
+        "editorial": {
+            "nombre": "Romances Editoriales",
+            "pais": "Argentina",
+            "anioFundacion": 2008
+        }
+    },
+    {
+        "titulo": "El ritual",
+        "genero": "terror",
+        "autores": ["Alejandro Gómez"],
+        "numeroPaginas": 410,
+        "nivelLectura": "adulto",
+        "precios": [
+            {"tipo": "fisico", "precio": 4300},
+            {"tipo": "digital", "precio": 3100}   
+        ],
+        "disponibleStock": false,
+        "editorial": {
+            "nombre": "La casita del horror",
+            "pais": "Mexico",
+            "anioFundacion": 1999
+        }
+    },
+    {
+        "titulo": "La isla misteriosa",
+        "genero": "aventura",
+        "autores": ["Lalo Landa", "Julio Verne"],
+        "numeroPaginas": 380,
+        "nivelLectura": "juvenil",
+        "precios": [
+            {"tipo": "fisico", "precio": 3900},
+            {"tipo": "digital", "precio": 2800}   
+        ],
+        "disponibleStock": true,
+        "editorial": {
+            "nombre": "Aventuras Editoriales",
+            "pais": "España",
+            "anioFundacion": 1975
+        }
+    },
+    {
+        "titulo": "Androides",
+        "genero": "ciencia ficcion",
+        "autores": ["Carlos Gómez"],
+        "numeroPaginas": 470,
+        "nivelLectura": "adulto",
+        "precios": [
+            {"tipo": "fisico", "precio": 4400},
+            {"tipo": "digital", "precio":3500}   
+        ],
+        "disponibleStock": false,
+        "editorial": {
+            "nombre": "Galaxia Libros",
+            "pais": "España",
+            "anioFundacion": 2012
+        }
+    }    
+])
